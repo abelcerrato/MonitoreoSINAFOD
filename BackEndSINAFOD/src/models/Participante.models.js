b36b267bf570c8b124d1522c80ead243ac2f32ae
Johana-Lara <lara26johana@gmail.com>
2025-05-21 11:01:17 -0600

    get paricipante con centroeducativo, formacion y la investigacion

--- BackEndSINAFOD/src/models/Participante.models.js
+++ BackEndSINAFOD/src/models/Participante.models.js
@@ -74,40 +74,108 @@ export const getParticipanteIdInvestM = async (id) => {
     try {
         const { rows } = await pool.query
             (`
-                SELECT p.idinvestigacion, i.investigacion, p.id as idparticipante, p.identificacion, p.codigosace, p.correo, p.nombre, p.fechanacimiento, p.edad, p.telefono, p.genero, 
-                    p.idnivelacademicos, n.nombre as nivelacademico,  p.idgradoacademicos, g.nombre as gradoacademico,
-                    p.añosdeservicio, p.codigodered, 
-                    p.deptoresidencia, d.nombre as departamento, p.municipioresidencia, m.nombre as municipio, p.aldearesidencia, a.nombre as aldea,  p.caserio, 
-                    p.idfuncion, c.cargo,
-                    p.datoscorrectos, p.autorizadatos, 
-                    mu.usuario as creadopor, p.fechacreacion, 
-                    mu2.usuario as modificadopor, p.fechamodificacion, 
-                    --datos del centro educativo
-                    ced.id as idcentroeducativo, ced.nombreced, ced.codigosace, ced.tipoadministracion, ced.tipocentro, ced.jornada, ced.zona, 
-	                ced.prebasica, ced.basica, ced.media, 
-	                ced.primero, ced.segundo, ced.tercero, ced.cuarto, ced.quinto, ced.sexto, ced.séptimo, ced.octavo, ced.noveno, 
-	                ced.btp1, ced.btp2, ced.btp3, ced.bch1, ced.bch2, ced.bch3, 
-	                ced.modalidad, 
-	                ced.iddepartamento, dced.nombre as departamentoced,
-	                ced.idmunicipio, mced.nombre  as municipioced,
-	                ced.idaldea, aced.nombre as aldeaced
-                FROM participantes p
-                left join nivelesacademicos n on p.idnivelacademicos = n.id 
-                left join gradosacademicos g on p.idgradoacademicos = g.id 
-                left join departamento d on p.deptoresidencia = d.id 
-                left join municipio m on p.municipioresidencia = m.id 
-                left join aldeas a on p.aldearesidencia = a.id 
-                left join investigacion i on p.idinvestigacion= i.id 
-                left join formacion f on p.idformacion = f.id 
-                left join cargodesempeña c on p.idfuncion = c.id
-                --centro educativo 
-                left join centroeducativo ced on ced.idparticipante = p.id 
-                left join departamento dced on ced.iddepartamento = dced.id
-	            left join municipio mced on ced.idmunicipio = mced.id 
-	            left join aldeas aced on ced.idaldea = aced.id
-                left join ms_usuarios mu on p.creadopor = mu.id 
-                left join ms_usuarios mu2 on p.modificadopor = mu2.id
-                where p.idinvestigacion =$1
+               SELECT 
+                p.idinvestigacion, 
+                i.investigacion, 
+                p.id as idparticipante, 
+                p.identificacion, 
+                p.codigosace, 
+                p.correo, 
+                p.nombre, 
+                p.fechanacimiento, 
+                p.edad, 
+                p.telefono, 
+                p.genero, 
+                p.idnivelacademicos, 
+                n.nombre as nivelacademico,  
+                p.idgradoacademicos, 
+                g.nombre as gradoacademico,
+                p.añosdeservicio, 
+                p.codigodered, 
+                p.deptoresidencia, 
+                d.nombre as departamento, 
+                p.municipioresidencia, 
+                m.nombre as municipio, 
+                p.aldearesidencia, 
+                a.nombre as aldea,  
+                p.caserio, 
+                p.idfuncion, 
+                c.cargo,
+                p.datoscorrectos, 
+                p.autorizadatos, 
+                mu.usuario as creadopor, 
+                p.fechacreacion, 
+                mu2.usuario as modificadopor, 
+                p.fechamodificacion, 
+
+                -- datos del centro educativo
+                ced.id as idcentroeducativo, 
+                ced.nombreced, 
+                ced.codigosace, 
+                ced.tipoadministracion, 
+                ced.tipocentro, 
+                ced.jornada, 
+                ced.zona, 
+
+                -- columnas booleanas individuales
+                ced.prebasica, ced.basica, ced.media, 
+                ced.primero, ced.segundo, ced.tercero, ced.cuarto, ced.quinto, 
+                ced.sexto, ced.séptimo, ced.octavo, ced.noveno, 
+                ced.btp1, ced.btp2, ced.btp3, 
+                ced.bch1, ced.bch2, ced.bch3, 
+
+                -- columnas nuevas calculadas
+                CONCAT_WS(', ',
+                    CASE WHEN ced.prebasica THEN 'Prebásica' END,
+                    CASE WHEN ced.basica THEN 'Básica' END,
+                    CASE WHEN ced.media THEN 'Media' END
+                ) AS nivelacademico_ced,
+
+                CONCAT_WS(', ',
+                    CASE WHEN ced.primero THEN 'Primero' END,
+                    CASE WHEN ced.segundo THEN 'Segundo' END,
+                    CASE WHEN ced.tercero THEN 'Tercero' END,
+                    CASE WHEN ced.cuarto THEN 'Cuarto' END,
+                    CASE WHEN ced.quinto THEN 'Quinto' END,
+                    CASE WHEN ced.sexto THEN 'Sexto' END,
+                    CASE WHEN ced.séptimo THEN 'Séptimo' END,
+                    CASE WHEN ced.octavo THEN 'Octavo' END,
+                    CASE WHEN ced.noveno THEN 'Noveno' END,
+                    CASE WHEN ced.btp1 THEN 'BTP 1' END,
+                    CASE WHEN ced.btp2 THEN 'BTP 2' END,
+                    CASE WHEN ced.btp3 THEN 'BTP 3' END,
+                    CASE WHEN ced.bch1 THEN 'BCH 1' END,
+                    CASE WHEN ced.bch2 THEN 'BCH 2' END,
+                    CASE WHEN ced.bch3 THEN 'BCH 3' END
+                ) AS gradoacademico_ced,
+
+                -- ubicación del centro educativo
+                ced.modalidad, 
+                ced.iddepartamento, dced.nombre as departamentoced,
+                ced.idmunicipio, mced.nombre as municipioced,
+                ced.idaldea, aced.nombre as aldeaced
+
+            FROM participantes p
+            LEFT JOIN nivelesacademicos n ON p.idnivelacademicos = n.id 
+            LEFT JOIN gradosacademicos g ON p.idgradoacademicos = g.id 
+            LEFT JOIN departamento d ON p.deptoresidencia = d.id 
+            LEFT JOIN municipio m ON p.municipioresidencia = m.id 
+            LEFT JOIN aldeas a ON p.aldearesidencia = a.id 
+            LEFT JOIN investigacion i ON p.idinvestigacion = i.id 
+            LEFT JOIN formacion f ON p.idformacion = f.id 
+            LEFT JOIN cargodesempeña c ON p.idfuncion = c.id
+
+            -- centro educativo
+            LEFT JOIN centroeducativo ced ON ced.idparticipante = p.id 
+            LEFT JOIN departamento dced ON ced.iddepartamento = dced.id
+            LEFT JOIN municipio mced ON ced.idmunicipio = mced.id 
+            LEFT JOIN aldeas aced ON ced.idaldea = aced.id
+
+            LEFT JOIN ms_usuarios mu ON p.creadopor = mu.id 
+            LEFT JOIN ms_usuarios mu2 ON p.modificadopor = mu2.id
+
+            WHERE p.idinvestigacion = $1;
+
 
         `, [id])
         //console.log(rows);
@@ -124,39 +192,105 @@ export const getParticipanteIdFormacionM = async (id) => {
     try {
         const { rows } = await pool.query
             (`
-             SELECT p.idformacion, f.formacion, p.id as idparticipante, p.identificacion, p.codigosace, p.correo, p.nombre, p.fechanacimiento, p.edad, p.telefono, p.genero, 
-                    p.idnivelacademicos, n.nombre as nivelacademico,  p.idgradoacademicos, g.nombre as gradoacademico,
-                    p.añosdeservicio, p.codigodered, 
-                    p.deptoresidencia, d.nombre as departamento, p.municipioresidencia, m.nombre as municipio, p.aldearesidencia, a.nombre as aldea,  p.caserio, 
-                    p.idfuncion, c.cargo,
-                    p.datoscorrectos, p.autorizadatos, 
-                    mu.usuario as creadopor, p.fechacreacion, 
-                    mu2.usuario as modificadopor, p.fechamodificacion, 
-                    --datos del centro educativo
-                    ced.id as idcentroeducativo, ced.nombreced, ced.codigosace, ced.tipoadministracion, ced.tipocentro, ced.jornada, ced.zona, 
-	                ced.prebasica, ced.basica, ced.media, 
-	                ced.primero, ced.segundo, ced.tercero, ced.cuarto, ced.quinto, ced.sexto, ced.séptimo, ced.octavo, ced.noveno, 
-	                ced.btp1, ced.btp2, ced.btp3, ced.bch1, ced.bch2, ced.bch3, 
-	                ced.modalidad, 
-	                ced.iddepartamento, dced.nombre as departamentoced,
-	                ced.idmunicipio, mced.nombre  as municipioced,
-	                ced.idaldea, aced.nombre as aldeaced
-                FROM participantes p
-                left join nivelesacademicos n on p.idnivelacademicos = n.id 
-                left join gradosacademicos g on p.idgradoacademicos = g.id 
-                left join departamento d on p.deptoresidencia = d.id 
-                left join municipio m on p.municipioresidencia = m.id 
-                left join aldeas a on p.aldearesidencia = a.id 
-                left join investigacion i on p.idinvestigacion= i.id 
-                left join formacion f on p.idformacion = f.id 
-                left join cargodesempeña c on p.idfuncion = c.id
-                --centro educativo 
-                left join centroeducativo ced on ced.idparticipante = p.id 
-                left join departamento dced on ced.iddepartamento = dced.id
-	            left join municipio mced on ced.idmunicipio = mced.id 
-	            left join aldeas aced on ced.idaldea = aced.id
-                left join ms_usuarios mu on p.creadopor = mu.id 
-                left join ms_usuarios mu2 on p.modificadopor = mu2.id
+             SELECT 
+                p.idformacion, 
+                f.formacion,
+                 p.id as idparticipante, 
+                p.identificacion, 
+                p.codigosace, 
+                p.correo, 
+                p.nombre, 
+                p.fechanacimiento, 
+                p.edad, 
+                p.telefono, 
+                p.genero, 
+                p.idnivelacademicos, 
+                n.nombre as nivelacademico,  
+                p.idgradoacademicos, 
+                g.nombre as gradoacademico,
+                p.añosdeservicio, 
+                p.codigodered, 
+                p.deptoresidencia, 
+                d.nombre as departamento, 
+                p.municipioresidencia, 
+                m.nombre as municipio, 
+                p.aldearesidencia, 
+                a.nombre as aldea,  
+                p.caserio, 
+                p.idfuncion, 
+                c.cargo,
+                p.datoscorrectos, 
+                p.autorizadatos, 
+                mu.usuario as creadopor, 
+                p.fechacreacion, 
+                mu2.usuario as modificadopor, 
+                p.fechamodificacion, 
+
+                -- datos del centro educativo
+                ced.id as idcentroeducativo, 
+                ced.nombreced, 
+                ced.codigosace, 
+                ced.tipoadministracion, 
+                ced.tipocentro, 
+                ced.jornada, 
+                ced.zona, 
+
+                -- columnas booleanas individuales
+                ced.prebasica, ced.basica, ced.media, 
+                ced.primero, ced.segundo, ced.tercero, ced.cuarto, ced.quinto, 
+                ced.sexto, ced.séptimo, ced.octavo, ced.noveno, 
+                ced.btp1, ced.btp2, ced.btp3, 
+                ced.bch1, ced.bch2, ced.bch3, 
+
+                -- columnas nuevas calculadas
+                CONCAT_WS(', ',
+                    CASE WHEN ced.prebasica THEN 'Prebásica' END,
+                    CASE WHEN ced.basica THEN 'Básica' END,
+                    CASE WHEN ced.media THEN 'Media' END
+                ) AS nivelacademico_ced,
+
+                CONCAT_WS(', ',
+                    CASE WHEN ced.primero THEN 'Primero' END,
+                    CASE WHEN ced.segundo THEN 'Segundo' END,
+                    CASE WHEN ced.tercero THEN 'Tercero' END,
+                    CASE WHEN ced.cuarto THEN 'Cuarto' END,
+                    CASE WHEN ced.quinto THEN 'Quinto' END,
+                    CASE WHEN ced.sexto THEN 'Sexto' END,
+                    CASE WHEN ced.séptimo THEN 'Séptimo' END,
+                    CASE WHEN ced.octavo THEN 'Octavo' END,
+                    CASE WHEN ced.noveno THEN 'Noveno' END,
+                    CASE WHEN ced.btp1 THEN 'BTP 1' END,
+                    CASE WHEN ced.btp2 THEN 'BTP 2' END,
+                    CASE WHEN ced.btp3 THEN 'BTP 3' END,
+                    CASE WHEN ced.bch1 THEN 'BCH 1' END,
+                    CASE WHEN ced.bch2 THEN 'BCH 2' END,
+                    CASE WHEN ced.bch3 THEN 'BCH 3' END
+                ) AS gradoacademico_ced,
+
+                -- ubicación del centro educativo
+                ced.modalidad, 
+                ced.iddepartamento, dced.nombre as departamentoced,
+                ced.idmunicipio, mced.nombre as municipioced,
+                ced.idaldea, aced.nombre as aldeaced
+
+            FROM participantes p
+            LEFT JOIN nivelesacademicos n ON p.idnivelacademicos = n.id 
+            LEFT JOIN gradosacademicos g ON p.idgradoacademicos = g.id 
+            LEFT JOIN departamento d ON p.deptoresidencia = d.id 
+            LEFT JOIN municipio m ON p.municipioresidencia = m.id 
+            LEFT JOIN aldeas a ON p.aldearesidencia = a.id 
+            LEFT JOIN investigacion i ON p.idinvestigacion = i.id 
+            LEFT JOIN formacion f ON p.idformacion = f.id 
+            LEFT JOIN cargodesempeña c ON p.idfuncion = c.id
+
+            -- centro educativo
+            LEFT JOIN centroeducativo ced ON ced.idparticipante = p.id 
+            LEFT JOIN departamento dced ON ced.iddepartamento = dced.id
+            LEFT JOIN municipio mced ON ced.idmunicipio = mced.id 
+            LEFT JOIN aldeas aced ON ced.idaldea = aced.id
+
+            LEFT JOIN ms_usuarios mu ON p.creadopor = mu.id 
+            LEFT JOIN ms_usuarios mu2 ON p.modificadopor = mu2.id
             where p.idformacion =$1
 
         `, [id])
